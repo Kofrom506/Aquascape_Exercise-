737145259eda1ea95a15ce092318b4809190e472
embunazzahra <dhauembunazzahra@gmail.com>
2022-05-05 01:13:49 +0700

    mkdir and add some themes

---
+++ lib/ui/pages/auto_page.dart
@@ -0,0 +1,15 @@
+import 'package:aquascape_exercise/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class AutoPage extends StatelessWidget {
+  const AutoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 329,
+      height: 76,
+      decoration: BoxDecoration(gradient: blueGradient),
+    );
+  }
+}
